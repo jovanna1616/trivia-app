56bfd10f80ae826f8157b83b167572bfec514677
jovanna1616 <jovana.r@vivifyideas.com>
2018-05-18 19:06:10 +0200

    Zadatak 2.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { router } from './router';
+import { store } from './store';
+
 
 Vue.use(BootstrapVue)
 
@@ -11,5 +13,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/store/ChuckModule.js
@@ -0,0 +1,8 @@
+export const ChuckModule = {
+  state: {
+  },
+  getters: {
+  },
+  mutations: {},
+  actions: {}
+}
